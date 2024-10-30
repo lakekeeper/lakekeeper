2b4c88a0cf0bc56e89dbf0e52b5c375df37b17a3
Christian Thiel <christian@hansetag.com>
2024-10-30 10:20:14 -0700

    Fix bearerAuth

--- crates/iceberg-catalog/src/api/management/mod.rs
+++ crates/iceberg-catalog/src/api/management/mod.rs
@@ -162,7 +162,7 @@ pub mod v1 {
         fn modify(&self, openapi: &mut utoipa::openapi::OpenApi) {
             let components = openapi.components.as_mut().unwrap(); // we can unwrap safely since there already is components registered.
             components.add_security_scheme(
-                "api_key",
+                "bearerAuth",
                 SecurityScheme::Http(
                     utoipa::openapi::security::HttpBuilder::new()
                         .scheme(utoipa::openapi::security::HttpAuthScheme::Bearer)
