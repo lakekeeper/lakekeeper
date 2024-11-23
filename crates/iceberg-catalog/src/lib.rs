1ae99583dc40b67a318b5b27f1a82ff7f1e57e87
Tobias Puetz <tobias@hansetag.com>
2024-11-23 14:45:09 +0000

    use a shared runtime for tests that share the static client

--- crates/iceberg-catalog/src/lib.rs
+++ crates/iceberg-catalog/src/lib.rs
@@ -23,3 +23,31 @@ pub mod api;
 pub mod metrics;
 #[cfg(feature = "router")]
 pub(crate) mod tracing;
+
+#[cfg(test)]
+pub mod test {
+    use std::future::Future;
+    use std::sync::LazyLock;
+    use tokio::runtime::Runtime;
+
+    static COMMON_RUNTIME: LazyLock<Runtime> = LazyLock::new(|| {
+        tokio::runtime::Builder::new_multi_thread()
+            .enable_all()
+            .build()
+            .expect("failed to start Tokio runtime")
+    });
+
+    #[track_caller]
+    pub(crate) fn test_block_on<F: Future>(f: F, common_runtime: bool) -> F::Output {
+        {
+            if common_runtime {
+                return COMMON_RUNTIME.block_on(f);
+            }
+            tokio::runtime::Builder::new_current_thread()
+                .enable_all()
+                .build()
+                .expect("failed to start Tokio runtime")
+                .block_on(f)
+        }
+    }
+}
