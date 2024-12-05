c6d85471855db0e87f7ff6f7f1ef1d0c5d8c2d6e
Christian Thiel <christian@hansetag.com>
2024-12-05 08:26:23 +0100

    Add caching

--- crates/iceberg-catalog-bin/src/ui.rs
+++ crates/iceberg-catalog-bin/src/ui.rs
@@ -24,6 +24,8 @@ const UI_CONFIG: LazyCell<LakekeeperConsoleConfig> = LazyCell::new(|| {
         idp_redirect_path: std::env::var("LAKEKEEPER__UI__IDP_REDIRECT_PATH")
             .unwrap_or(default_config.idp_redirect_path),
         idp_scope: std::env::var("LAKEKEEPER__UI__IDP_SCOPE").unwrap_or(default_config.idp_scope),
+        idp_resource: std::env::var("LAKEKEEPER__UI__IDP_RESOURCE")
+            .unwrap_or(default_config.idp_resource),
         idp_post_logout_redirect_path: std::env::var(
             "LAKEKEEPER__UI__IDP_POST_LOGOUT_REDIRECT_PATH",
         )
@@ -37,6 +39,20 @@ const UI_CONFIG: LazyCell<LakekeeperConsoleConfig> = LazyCell::new(|| {
                 .to_string(),
         ),
     }
+});
+
+#[derive(Debug, Clone)]
+enum CacheItem {
+    NotFound,
+    Found {
+        mime: mime_guess::Mime,
+        data: std::borrow::Cow<'static, [u8]>,
+    },
+}
+
+const FILE_CACHE: LazyCell<moka::sync::Cache<String, CacheItem>> = LazyCell::new(|| {
+    let cache = moka::sync::Cache::new(1000);
+    cache
 });
 
 // We use static route matchers ("/" and "/index.html") to serve our home
@@ -58,7 +74,40 @@ pub async fn static_handler(uri: Uri) -> impl IntoResponse {
     StaticFile(path)
 }
 
+fn get_file_cached(file_path: &str) -> Response {
+    let cached = FILE_CACHE.get(file_path);
+
+    if let Some(cache_item) = cached {
+        cache_item.into_response()
+    } else {
+        let mime = mime_guess::from_path(file_path).first_or_octet_stream();
+        let content = get_file(file_path, &UI_CONFIG);
+
+        let cache_item = match content {
+            Some(content) => CacheItem::Found {
+                mime: mime.clone(),
+                data: content.data,
+            },
+            None => CacheItem::NotFound,
+        };
+        FILE_CACHE.insert(file_path.to_string(), cache_item.clone());
+
+        cache_item.into_response()
+    }
+}
+
 pub struct StaticFile<T>(pub T);
+
+impl IntoResponse for CacheItem {
+    fn into_response(self) -> Response {
+        match self {
+            CacheItem::NotFound => (StatusCode::NOT_FOUND, "404 Not Found").into_response(),
+            CacheItem::Found { mime, data } => {
+                ([(header::CONTENT_TYPE, mime.as_ref())], data).into_response()
+            }
+        }
+    }
+}
 
 impl<T> IntoResponse for StaticFile<T>
 where
@@ -66,14 +115,7 @@ where
 {
     fn into_response(self) -> Response {
         let path = self.0.into();
-
-        match get_file(path.as_str(), &UI_CONFIG) {
-            Some(content) => {
-                let mime = mime_guess::from_path(path).first_or_octet_stream();
-                ([(header::CONTENT_TYPE, mime.as_ref())], content.data).into_response()
-            }
-            None => (StatusCode::NOT_FOUND, "404 Not Found").into_response(),
-        }
+        get_file_cached(&path)
     }
 }
 
