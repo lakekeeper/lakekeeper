4a2b5a5b418710bdcfe5ce5d0abc566a84ccf785
Tobias Puetz <tobias@hansetag.com>
2024-12-17 16:33:54 +0100

    feat(tests): run against multiple iceberg versions

--- tests/run.sh
+++ tests/run.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 # This script is meant to run in  apache/spark:3.5.1-java17-python3 like docker images
-set -e
+set -euo pipefail
 
 source common.sh
 
 setup_python
 
+TOX_NAME="${1%%-*}"
+SPARK_VERSION="${1#*-}"
+
+# unset if empty else export
+if [ "$SPARK_VERSION" != "$TOX_NAME" ]; then
+  export LAKEKEEPER_TEST__SPARK_ICEBERG_VERSION="$SPARK_VERSION"
+fi
+
+
 # Running tests
 echo "Running tests ..."
 cd python
-tox -qe "${1}"
+tox -qe "${TOX_NAME}"
