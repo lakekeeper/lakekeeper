8a93e7192e56221dd689db80fc1f0efe71f26c45
Tobias Puetz <tobias@hansetag.com>
2025-03-27 12:14:24 +0100

    fix remaining tests

--- tests/python/tests/test_trino.py
+++ tests/python/tests/test_trino.py
@@ -120,7 +120,7 @@ def test_rename_table(trino, warehouse: conftest.Warehouse):
            ) in warehouse.pyiceberg_catalog.list_tables("test_rename_table_trino")
 
 
-def tets_create_view(trino, warehouse: conftest.Warehouse):
+def test_create_view(trino, warehouse: conftest.Warehouse):
     cur = trino.cursor()
     cur.execute("CREATE SCHEMA test_create_view_trino")
     cur.execute(
@@ -129,15 +129,14 @@ def tets_create_view(trino, warehouse: conftest.Warehouse):
     cur.execute(
         "CREATE OR REPLACE VIEW test_create_view_trino.my_view AS SELECT strings FROM test_create_view_trino.my_table"
     )
-    assert ("my_view",) in warehouse.pyiceberg_catalog.list_tables(
-        "test_create_view_trino"
-    )
+    assert ["my_view"] in cur.execute(f"SHOW TABLES IN test_create_view_trino").fetchall()
+
     # Insert data and query view
     cur.execute(
         "INSERT INTO test_create_view_trino.my_table VALUES (1, 1.0, 'a'), (2, 2.0, 'b')"
     )
     r = cur.execute("SELECT * FROM test_create_view_trino.my_view").fetchall()
-    assert r == [("a",), ("b",)]
+    assert r == [["a"], ["b"]]
 
 
 def test_replace_view(trino):
@@ -156,7 +155,7 @@ def test_replace_view(trino):
         f"INSERT INTO {ns}.my_table VALUES (1, 1.0, 'a'), (2, 2.0, 'b')"
     )
     r = cur.execute(f"SELECT * FROM {ns}.my_view").fetchall()
-    assert r == [("a",), ("b",)]
+    assert r == [["a"], ["b"]]
 
     cur.execute(
         f"CREATE OR REPLACE VIEW {ns}.my_view AS SELECT strings FROM {ns}.my_table"
