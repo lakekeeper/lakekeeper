7f0c6da0e9add3027c74a5f7b2f34072002dbc76
Marius Kriegerowski <marius.kriegerowski@gmail.com>
2025-05-15 17:05:56 +0200

    extend metrics table

--- crates/iceberg-catalog/src/implementations/postgres/metrics.rs
+++ crates/iceberg-catalog/src/implementations/postgres/metrics.rs
@@ -6,16 +6,25 @@ pub(crate) async fn create_metric(
     report_metrics_request: ReportMetricsRequest,
     transaction: &mut sqlx::Transaction<'_, sqlx::Postgres>,
 ) -> Result<()> {
-    let _ = sqlx::query_scalar!(
-        r#"
-        INSERT INTO table_metrics (field)
-        VALUES ($1)
-        "#,
-        "Foo"
-    )
-    .fetch_one(&mut **transaction)
-    .await
-    .map_err(|e| {
+    let query = match report_metrics_request {
+        ReportMetricsRequest::ScanReport(_) => {
+            todo!()
+        }
+        ReportMetricsRequest::CommitReport(report) => {
+            sqlx::query_scalar!(
+                r#"
+                INSERT INTO table_metrics_commit_report (field)
+                VALUES ($1)
+                "#,
+                "Foo"
+            )
+        }
+        ReportMetricsRequest::DatabaseTest(_) => {
+            todo!()
+        }
+    };
+
+    query.fetch_one(&mut **transaction).await.map_err(|e| {
         tracing::warn!(?e, "Error");
         e.into_error_model(format!("Error creating metric"))
     })?;
