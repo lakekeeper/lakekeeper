d3c05794db77841038e796b3f98d76a6dd9e4c07
Moritz <moritz.zielke@gmail.com>
2025-07-10 15:28:19 +0200

    Modularize spark.py

--- tests/migrations/spark.py
+++ tests/migrations/spark.py
@@ -1,3 +1,5 @@
+import argparse
+import sys
 import time
 
 import pyspark
@@ -5,62 +7,87 @@ from pyspark.conf import SparkConf
 from pyspark.sql import SparkSession
 from pyspark.sql.types import FloatType, LongType, StructType, StructField, StringType
 
-# This CATALOG_URL works for the "docker compose" testing and development environment.
-# Change it if you are not running on `docker compose`.
-CATALOG_URL = "http://lakekeeper_initial:8181/catalog"
-WAREHOUSE = "demo"
+def spark_session():
+    """
+    Creates and returns a spark session.
+    """
+    # This CATALOG_URL works for the "docker compose" testing and development environment.
+    # Change it if you are not running on `docker compose`.
+    CATALOG_URL = "http://lakekeeper_initial:8181/catalog"
+    WAREHOUSE = "demo"
 
-SPARK_VERSION = pyspark.__version__
-SPARK_MINOR_VERSION = '.'.join(SPARK_VERSION.split('.')[:2])
-ICEBERG_VERSION = "1.6.1"
+    SPARK_VERSION = pyspark.__version__
+    SPARK_MINOR_VERSION = '.'.join(SPARK_VERSION.split('.')[:2])
+    ICEBERG_VERSION = "1.6.1"
 
-config = {
-    f"spark.sql.catalog.lakekeeper": "org.apache.iceberg.spark.SparkCatalog",
-    f"spark.sql.catalog.lakekeeper.type": "rest",
-    f"spark.sql.catalog.lakekeeper.uri": CATALOG_URL,
-    f"spark.sql.catalog.lakekeeper.warehouse": WAREHOUSE,
-    f"spark.sql.catalog.lakekeeper.io-impl": "org.apache.iceberg.aws.s3.S3FileIO",
-    "spark.sql.extensions": "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions",
-    "spark.sql.defaultCatalog": "lakekeeper",
-    "spark.jars.packages": f"org.apache.iceberg:iceberg-spark-runtime-{SPARK_MINOR_VERSION}_2.12:{ICEBERG_VERSION},org.apache.iceberg:iceberg-aws-bundle:{ICEBERG_VERSION}",
-}
-spark_config = SparkConf().setMaster('local').setAppName("Iceberg-REST")
-for k, v in config.items():
-    spark_config = spark_config.set(k, v)
-spark = SparkSession.builder.config(conf=spark_config).getOrCreate()
+    config = {
+        f"spark.sql.catalog.lakekeeper": "org.apache.iceberg.spark.SparkCatalog",
+        f"spark.sql.catalog.lakekeeper.type": "rest",
+        f"spark.sql.catalog.lakekeeper.uri": CATALOG_URL,
+        f"spark.sql.catalog.lakekeeper.warehouse": WAREHOUSE,
+        f"spark.sql.catalog.lakekeeper.io-impl": "org.apache.iceberg.aws.s3.S3FileIO",
+        "spark.sql.extensions": "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions",
+        "spark.sql.defaultCatalog": "lakekeeper",
+        "spark.jars.packages": f"org.apache.iceberg:iceberg-spark-runtime-{SPARK_MINOR_VERSION}_2.12:{ICEBERG_VERSION},org.apache.iceberg:iceberg-aws-bundle:{ICEBERG_VERSION}",
+    }
+    spark_config = SparkConf().setMaster('local').setAppName("Iceberg-REST")
+    for k, v in config.items():
+        spark_config = spark_config.set(k, v)
+    spark = SparkSession.builder.config(conf=spark_config).getOrCreate()
 
-spark.sql("USE lakekeeper")
-spark.sql("CREATE NAMESPACE IF NOT EXISTS my_namespace")
-spark.sql("SHOW NAMESPACES").show()
+    spark.sql("USE lakekeeper")
+    spark.sql("CREATE NAMESPACE IF NOT EXISTS my_namespace")
+    spark.sql("SHOW NAMESPACES").show()
 
-# Lakekeeper migration issues can be related to (soft) deleted tables.
-# So create and drop some tables to simulate that situation.
-tables = ["my_table_0", "my_table_1", "my_table_2"]
-schema = StructType([
-  StructField("id", LongType(), True),
-  StructField("strings", StringType(), True),
-  StructField("floats", FloatType(), True),
-])
-for table in tables:
-    df = spark.createDataFrame([], schema)
-    df.writeTo(f"my_namespace.{table}").createOrReplace()
+    return spark
 
-    # Insert some rows.
-    schema = spark.table(f"my_namespace.{table}").schema
-    data = [
-        [1, 'a-string', 2.2],
-        [2, 'b-string', 3.3]
-    ]
-    df = spark.createDataFrame(data, schema)
-    df.writeTo(f"my_namespace.{table}").append()
+def write_data(spark):
+    """
+    Creates tables and drops some of them for the provided spark session.
+    """
+    # Lakekeeper migration issues can be related to (soft) deleted tables.
+    # So create and drop some tables to simulate that situation.
+    tables = ["my_table_0", "my_table_1", "my_table_2"]
+    schema = StructType([
+      StructField("id", LongType(), True),
+      StructField("strings", StringType(), True),
+      StructField("floats", FloatType(), True),
+    ])
+    for table in tables:
+        df = spark.createDataFrame([], schema)
+        df.writeTo(f"my_namespace.{table}").createOrReplace()
 
-    spark.sql(f"SELECT * FROM my_namespace.{table}").show()
+        # Insert some rows.
+        schema = spark.table(f"my_namespace.{table}").schema
+        data = [
+            [1, 'a-string', 2.2],
+            [2, 'b-string', 3.3]
+        ]
+        df = spark.createDataFrame(data, schema)
+        df.writeTo(f"my_namespace.{table}").append()
 
-# Use all `DROP` variants to delete some of tables.
-# Leave at least one table undropped.
-spark.sql(f"DROP TABLE my_namespace.{tables[0]}")
-# TODO(mooori) permissions not sufficient for PURGE. Do we need it?
-# spark.sql(f"DROP TABLE my_namespace.{tables[1]} PURGE")
+        spark.sql(f"SELECT * FROM my_namespace.{table}").show()
 
-# Sleep to let (short) soft-delete timeout expire.
-time.sleep(3)
+    # Use all `DROP` variants to delete some of tables.
+    # Leave at least one table undropped.
+    spark.sql(f"DROP TABLE my_namespace.{tables[0]}")
+
+    # Sleep to let (short) soft-delete timeout expire.
+    time.sleep(3)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "task",
+        choices = ["write_data", "read_data"]
+    )
+    args = parser.parse_args()
+
+    if args.task == "write_data":
+        spark = spark_session()
+        write_data(spark)
+
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
