309a61d5f3a205879bc42267ca79c900a53dd4fd
Christian Thiel <christian@hansetag.com>
2025-09-23 11:00:22 +0200

    clippy

--- crates/lakekeeper/src/service/authz/implementations/openfga/migration/migration_fns_v4.rs
+++ crates/lakekeeper/src/service/authz/implementations/openfga/migration/migration_fns_v4.rs
@@ -70,6 +70,7 @@ static OPENFGA_REQ_PERMITS: LazyLock<Arc<Semaphore>> =
     LazyLock::new(|| Arc::new(Semaphore::const_new(50)));
 
 #[tracing::instrument(skip(client), fields(store_name = %state.store_name, server_id = %state.server_id))]
+#[allow(clippy::used_underscore_binding)]
 pub(crate) async fn v4_push_down_warehouse_id(
     mut client: BasicOpenFgaServiceClient,
     _prev_auth_model_id: Option<String>,
