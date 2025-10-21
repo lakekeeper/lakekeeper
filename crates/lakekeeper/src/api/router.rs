f5d10535cfc2212b028ea0c08bc115be371d64c5
Christian Thiel <christian@vakamo.com>
2025-10-21 07:52:04 +0200

    clippy

--- crates/lakekeeper/src/api/router.rs
+++ crates/lakekeeper/src/api/router.rs
@@ -4,7 +4,6 @@ use axum::{response::IntoResponse, routing::get, Json, Router};
 use axum_extra::{either::Either, middleware::option_layer};
 use axum_prometheus::PrometheusMetricLayer;
 use http::{header, HeaderName, HeaderValue, Method};
-use jsonwebtoken::Header;
 use limes::Authenticator;
 use tower::ServiceBuilder;
 use tower_http::{
