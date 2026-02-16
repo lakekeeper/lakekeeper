a4a677d9e14a4534d38024d496d0b337e81fb601
Patrick Boerzel <patrick@hansetag.com>
2026-02-16 15:25:04 +0100

    Implement StorageLayout System

--- crates/lakekeeper/src/service/storage/mod.rs
+++ crates/lakekeeper/src/service/storage/mod.rs
@@ -6,7 +6,7 @@ pub mod error;
 pub(crate) mod gcs;
 pub mod s3;
 
-use std::{collections::HashMap, str::FromStr as _};
+use std::{borrow::Cow, collections::HashMap, str::FromStr as _};
 
 pub use az::{AdlsProfile, AzCredential};
 pub(crate) use error::ValidationError;
@@ -21,6 +21,8 @@ use lakekeeper_io::{
 };
 pub use s3::{S3Credential, S3Flavor, S3Profile};
 use serde::{Deserialize, Serialize};
+use strum::Display;
+use uuid::Uuid;
 
 use super::{NamespaceId, TableId, secrets::SecretInStorage};
 use crate::{
@@ -1056,6 +1058,124 @@ pub(crate) async fn is_empty(
     Ok(true)
 }
 
+#[derive(Debug, Display, thiserror::Error)]
+pub enum StorageLayoutError {
+    InvalidTemplate(String),
+}
+
+pub trait PathSegmentContext {
+    fn get_name(&self) -> Cow<'_, str>;
+
+    fn get_uuid(&self) -> Uuid;
+}
+
+pub trait TemplatedPathSegmentRenderer {
+    type Context: PathSegmentContext;
+
+    fn template(&self) -> Cow<'_, str>;
+
+    fn render(&self, context: &Self::Context) -> String {
+        let template = self.template();
+        let uuid = context.get_uuid();
+        let name = context.get_name();
+        template
+            .replace("{uuid}", &uuid.to_string())
+            .replace("{name}", &name)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct NamespaceNameContext {
+    pub name: String,
+    pub uuid: Uuid,
+}
+
+impl PathSegmentContext for NamespaceNameContext {
+    fn get_name(&self) -> Cow<'_, str> {
+        Cow::Borrowed(&self.name)
+    }
+
+    fn get_uuid(&self) -> Uuid {
+        self.uuid
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct NamespaceNameRenderer(String);
+
+impl TemplatedPathSegmentRenderer for NamespaceNameRenderer {
+    type Context = NamespaceNameContext;
+
+    fn template(&self) -> Cow<'_, str> {
+        Cow::Borrowed(&self.0)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct TableNameContext {
+    pub name: String,
+    pub uuid: Uuid,
+}
+
+impl PathSegmentContext for TableNameContext {
+    fn get_name(&self) -> Cow<'_, str> {
+        Cow::Borrowed(&self.name)
+    }
+
+    fn get_uuid(&self) -> Uuid {
+        self.uuid
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct TableNameRenderer(String);
+
+impl TemplatedPathSegmentRenderer for TableNameRenderer {
+    type Context = TableNameContext;
+
+    fn template(&self) -> Cow<'_, str> {
+        Cow::Borrowed(&self.0)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum StorageLayout {
+    Flat(TableNameRenderer),
+    Parent(NamespaceNameRenderer, TableNameRenderer),
+    Full(NamespaceNameRenderer, TableNameRenderer),
+}
+
+impl StorageLayout {
+    pub fn try_new_flat(table_template: String) -> Result<Self, StorageLayoutError> {
+        if !table_template.contains("{uuid}") {
+            return Err(StorageLayoutError::InvalidTemplate(format!(
+                "For the 'Flat' layout, the table template '{table_template}' must contain the {{uuid}} placeholder to prevent path collisions."
+            )));
+        }
+        Ok(Self::Flat(TableNameRenderer(table_template)))
+    }
+
+    pub fn try_new_parent(
+        namespace_template: String,
+        table_template: String,
+    ) -> Result<Self, StorageLayoutError> {
+        Ok(Self::Parent(
+            NamespaceNameRenderer(namespace_template),
+            TableNameRenderer(table_template),
+        ))
+    }
+
+    pub fn try_new_full(
+        namespace_template: String,
+        table_template: String,
+    ) -> Result<Self, StorageLayoutError> {
+        Ok(Self::Full(
+            NamespaceNameRenderer(namespace_template),
+            TableNameRenderer(table_template),
+        ))
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use std::{collections::HashMap, str::FromStr};
@@ -1616,5 +1736,90 @@ mod tests {
         Box::pin(profile.validate_access(cred.as_ref(), None, &request_metadata))
             .await
             .unwrap();
+    }
+
+    #[test]
+    fn test_storage_layout_renders_flat_table_format_with_name_and_uuid() {
+        let template = "{name}-{uuid}";
+        let layout = StorageLayout::try_new_flat(template.to_string()).unwrap();
+        let context = TableNameContext {
+            name: "my_table".to_string(),
+            uuid: Uuid::new_v4(),
+        };
+
+        let StorageLayout::Flat(renderer) = layout else {
+            panic!("Expected flat storage layout");
+        };
+
+        assert_eq!(
+            renderer.render(&context),
+            format!("{}-{}", context.name, context.uuid)
+        );
+    }
+
+    #[test]
+    fn test_storage_layout_renders_parent_namespace_layout_with_namespace_name_and_uuid_and_table_name_and_uuid()
+     {
+        let template = "{name}-{uuid}";
+        let layout =
+            StorageLayout::try_new_parent(template.to_string(), template.to_string()).unwrap();
+        let table_context = TableNameContext {
+            name: "my_table".to_string(),
+            uuid: Uuid::new_v4(),
+        };
+        let namespace_context = NamespaceNameContext {
+            name: "my_namespace".to_string(),
+            uuid: Uuid::new_v4(),
+        };
+
+        let StorageLayout::Parent(namespace_renderer, table_renderer) = layout else {
+            panic!("Expected parent storage layout");
+        };
+
+        assert_eq!(
+            table_renderer.render(&table_context),
+            format!("{}-{}", table_context.name, table_context.uuid)
+        );
+        assert_eq!(
+            namespace_renderer.render(&namespace_context),
+            format!("{}-{}", namespace_context.name, namespace_context.uuid)
+        );
+    }
+
+    #[test]
+    fn test_storage_layout_renders_full_layout_with_namespace_name_and_uuid_and_table_name_and_uuid()
+     {
+        let template = "{name}-{uuid}";
+        let layout =
+            StorageLayout::try_new_full(template.to_string(), template.to_string()).unwrap();
+        let table_context = TableNameContext {
+            name: "my_table".to_string(),
+            uuid: Uuid::new_v4(),
+        };
+        let namespace_context = NamespaceNameContext {
+            name: "my_namespace".to_string(),
+            uuid: Uuid::new_v4(),
+        };
+
+        let StorageLayout::Full(namespace_renderer, table_renderer) = layout else {
+            panic!("Expected full storage layout");
+        };
+
+        assert_eq!(
+            table_renderer.render(&table_context),
+            format!("{}-{}", table_context.name, table_context.uuid)
+        );
+        assert_eq!(
+            namespace_renderer.render(&namespace_context),
+            format!("{}-{}", namespace_context.name, namespace_context.uuid)
+        );
+    }
+
+    #[test]
+    fn test_storage_layout_table_in_flat_layout_need_uuid() {
+        let invalid_template = "{name}";
+        let layout = StorageLayout::try_new_flat(invalid_template.to_string());
+        let layout = layout.expect_err("Expected error due to missing {uuid} in template");
+        assert!(matches!(layout, StorageLayoutError::InvalidTemplate(_)));
     }
 }
